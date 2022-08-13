a490c51f4ba6985a64c346594b80c0eed3382d82
nutstache <oma2668@gmail.com>
2022-08-13 18:17:36 +0530

    unicafe 1.6 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,34 @@
 import { useState } from 'react'
+const incrementState = (state, setState) => {
+  //console.log('Setting ',state)
+  setState(state => state + 1)
+}
+
+const Button = ({eventHandle, text}) => {
+  //console.log("In button")
+  
+  return(
+    <button onClick={eventHandle}>{text}</button>
+  )
+}
+
+const DisplayState = ({state, text}) => {
+  return(
+    <div>{text} {state}</div>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  //console.log(good,neutral,bad)
+
+  return (
+    <>
+      <DisplayState state={good} text='good'/>
+      <DisplayState state={neutral} text='neutral'/>
+      <DisplayState state={bad} text='bad'/>
+    </>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -8,7 +38,11 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button eventHandle={() => incrementState(good, setGood)} text='good'/>
+      <Button eventHandle={() => incrementState(neutral, setNeutral)} text='neutral'/>
+      <Button eventHandle={() => incrementState(bad, setBad)} text='bad'/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
