920984c55b345bcf236c5a0845a54b27691182b5
nutstache <oma2668@gmail.com>
2022-08-15 19:19:33 +0530

    phonebook 2.9 - filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,46 @@
 import { useState } from 'react'
 
+const Header = ({value}) => {
+  return(
+    <h2>{value}</h2>
+  )
+}
+
 const Persons = ({ persons }) => {
   return (
     <ul>
       {persons.map(person =>
-        <li key={person.name}>{person.name} {person.number}</li>)
+        <li key={person.id}>{person.name} {person.number}</li>)
       }
     </ul>
   )
 }
 
+const Filter = ({value, onChange}) => {
+  return (
+    <div>
+      filter shown with <input value={value} onChange={onChange}/>
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setFilterValue] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     //console.log('Event submit in addName ',event);
 
-    const newPersonObject = { name: newName, number: newNumber }
+    const newPersonObject = { name: newName, number: newNumber, id: persons.length + 1 }
     //console.log('new name',newNameObject)
     if (persons.find(person => person.name === newPersonObject.name))
       alert(`${newPersonObject.name} is already added to phonebook`)
@@ -36,9 +55,13 @@ const App = () => {
     setValue(event.target.value)
   }
 
+  const filteredResults = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
+  
   return (
     <div>
-      <h2>Phonebook</h2>
+      <Header value='Phonebook' />
+      <Filter value={filterValue} onChange={(event) => handleNewInputChange(event, setFilterValue)} />
+      <Header value='add a new' />
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={(event) => handleNewInputChange(event, setNewName)} />
@@ -50,8 +73,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <Header value='Numbers' />
+      <Persons persons={filteredResults} />
     </div>
   )
 }
