c9f5ab8f2a2f8013db87a569fb0abab7917e2fc2
Om Anand <oma2668@gmail.com>
2023-08-21 06:04:20 -0300

    Exercise 1.6

--- vite/unicafe/src/App.jsx
+++ vite/unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const Title = ({ title }) => <h1>{title}</h1>
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const StatisticRow = ({ counter, text }) => <div>{text} {counter}</div>
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <StatisticRow counter={good} text="good" />
+      <StatisticRow counter={neutral} text="neutral" />
+      <StatisticRow counter={bad} text="bad" />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +23,12 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Title title='give feedback' />
+      <Button handleClick={() => { setGood(good + 1) }} text='good' />
+      <Button handleClick={() => { setNeutral(neutral + 1) }} text='neutral' />
+      <Button handleClick={() => { setBad(bad + 1) }} text='bad' />
+      <Title title='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
