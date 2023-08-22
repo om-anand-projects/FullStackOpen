928e70891f214a4fd2aed7e70fb91864077cbca8
Om Anand <oma2668@gmail.com>
2023-08-22 01:25:55 -0300

    Ex 2.7

--- vite/phonebook/src/App.jsx
+++ vite/phonebook/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
@@ -9,7 +8,12 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    const newPerson = {name: newName}  
+    if (persons.map(person => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const newPerson = {name: newName}
+    // console.log(persons, newPerson, persons.includes(newPerson) )
     setPersons(persons.concat(newPerson))
     setNewName('')
   }
