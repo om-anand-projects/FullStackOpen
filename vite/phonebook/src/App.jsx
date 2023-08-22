4f8e197f119dc839a65ae909c301db05df93380b
Om Anand <oma2668@gmail.com>
2023-08-22 02:01:56 -0300

    Ex 2.9

--- vite/phonebook/src/App.jsx
+++ vite/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-const Input = ({ inputName, inputValue, onChange }) => {
+const Input = ({ inputText, inputValue, onChange }) => {
   return (
     <div>
-      {inputName}: <input value={inputValue} onChange={onChange} />
+      {inputText}: <input value={inputValue} onChange={onChange} />
     </div>
   )
 }
@@ -16,11 +16,15 @@ const PersonsList = ({ persons }) => {
 }
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchText, setSearchText] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -37,19 +41,26 @@ const App = () => {
 
   const handleNewNameChange = (event) => setNewName(event.target.value)
   const handleNewNumberChange = (event) => setNewNumber(event.target.value)
+  const handleNewSearchTextChange = (event) => setSearchText(event.target.value)
+
+  const filteredPersons = () => {
+    return persons.filter(person => person.name.toLowerCase().includes(searchText.toLowerCase()))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Input inputText="filter shown with " inputValue={searchText} onChange={handleNewSearchTextChange} />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
-        <Input inputName="name" inputValue={newName} onChange={handleNewNameChange} />
-        <Input inputName="number" inputValue={newNumber} onChange={handleNewNumberChange} />
+        <Input inputText="name" inputValue={newName} onChange={handleNewNameChange} />
+        <Input inputText="number" inputValue={newNumber} onChange={handleNewNumberChange} />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <PersonsList persons={persons}/>
+      <PersonsList persons={filteredPersons()} />
     </div>
   )
 }
