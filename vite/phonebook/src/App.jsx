2dcc03ac963b9efa0c2b65d6955ec565c58b1201
Om Anand <oma2668@gmail.com>
2023-08-22 01:14:53 -0300

    Ex 2.6

---
+++ vite/phonebook/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {name: newName}  
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+}
+
+export default App
