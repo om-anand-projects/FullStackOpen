c47ad684a78f0dd4344c841f715ba06c650c5beb
Om Anand <oma2668@gmail.com>
2023-08-22 03:17:59 -0300

    Ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PersonsList from './components/PersonsList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+
+  const getPersonsHook = () => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+  useEffect(getPersonsHook, [])
 
   const addName = (event) => {
     event.preventDefault()
@@ -34,7 +39,7 @@ const App = () => {
   }
 
   const filteredResults = persons.filter(person => person.name.toLowerCase().includes(filterValue.toLowerCase()))
-  
+
   const inputs = {
     handleChange: handleNewInputChange,
     inputValues: [
