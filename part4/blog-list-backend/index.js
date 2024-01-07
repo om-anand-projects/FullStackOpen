54004175dcaf99a3aae729e348d3534a5f56ba11
Om Anand <oma2668@gmail.com>
2024-01-06 22:03:53 -0400

    Separate routes

--- part4/blog-list-backend/index.js
+++ part4/blog-list-backend/index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
-const Blog = require('./model/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.mongoUrl)
 
@@ -14,23 +14,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
 
