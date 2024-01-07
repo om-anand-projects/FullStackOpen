cb0b1cbf8c8b18c3d3bde275ea673303071cfec3
Om Anand <oma2668@gmail.com>
2024-01-06 21:58:34 -0400

    Mongo model

--- part4/blog-list-backend/index.js
+++ part4/blog-list-backend/index.js
@@ -6,15 +6,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./model/blog')
 
 mongoose.connect(config.mongoUrl)
 
