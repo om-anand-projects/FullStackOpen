0804a5900e32395f7c8cd4f676aa8796c7ac968b
Om Anand <oma2668@gmail.com>
2024-01-06 22:10:08 -0400

    Separate server and app

---
+++ part4/blog-list-backend/app.js
@@ -0,0 +1,19 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
--- part4/blog-list-backend/index.js
+++ part4/blog-list-backend/index.js
@@ -1,24 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
-mongoose.connect(config.mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-
-const PORT = process.env.PORT
 app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+    logger.info(`Server running on port ${config.PORT}`)
 })
