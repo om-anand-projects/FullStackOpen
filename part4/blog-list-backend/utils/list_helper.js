c929cf868fab834dddad027b0a50530852442d5e
Om Anand <oma2668@gmail.com>
2024-01-09 21:07:05 -0400

    Favourite blog util and test (Ex 4.5)

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -4,14 +4,36 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-    const reducer = (sum, item) => {
+    const sumLikesReducer = (sum, item) => {
         return sum + item['likes']
     }
 
-    return blogs.reduce(reducer, 0)
+    return blogs.reduce(sumLikesReducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if (!blogs)
+        return null
+
+    const mostLikedBlog = {
+        title: null,
+        author: null,
+        likes: -1
+    }
+
+    blogs.forEach(blog => {
+        if (blog['likes'] > mostLikedBlog['likes']) {
+            mostLikedBlog['likes'] = blog['likes']
+            mostLikedBlog['author'] = blog['author']
+            mostLikedBlog['title'] = blog['title']
+        }
+    })
+
+    return mostLikedBlog
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
